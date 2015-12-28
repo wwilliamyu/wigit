3da4ea1ce840a3f17c30503cb0262de2a8d5781d
Jason Holtkamp <jasonholtkamp@Amy-Holtkamps-MacBook-Pro.local>
2015-12-28 16:35:06 -0600

    First Commit

---
+++ Wigit/Wigit/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Wigit
+//
+//  Created by Jason Holtkamp on 12/26/15.
+//  Copyright © 2015 Wigit, inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Wigit/Wigit/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Wigit
+//
+//  Created by Jason Holtkamp on 12/26/15.
+//  Copyright © 2015 Wigit, inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
