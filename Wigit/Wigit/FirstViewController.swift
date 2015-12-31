fbfa5adb40b1e01f94856bffcc24651aaa2269e5
wwilliamyu <wwilliamyu@gmail.com>
2015-12-31 02:10:32 -0800

    changed a few more things about login screen, should talk to lloyd about further changes

--- Wigit/Wigit/FirstViewController.swift
+++ Wigit/Wigit/FirstViewController.swift
@@ -11,6 +11,29 @@ import UIKit
 class FirstViewController: UIViewController {
     
     
+    @IBOutlet var username: UITextField!
+    @IBOutlet var password: UITextField!
+
+    @IBOutlet var loginButton: UIButton!
+    @IBOutlet var signupButton: UIButton!
+    
+    @IBAction func login(sender: AnyObject) {
+        if  (username.text == "" || password.text == "") {
+            let alert = UIAlertController(title: "Alert", message: "Message", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+        
+    }
+
+
+    @IBAction func signup(sender: AnyObject) {
+        if  (username.text == "" || password.text == "") {
+            
+        }
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
