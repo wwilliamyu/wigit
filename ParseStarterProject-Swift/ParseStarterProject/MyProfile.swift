770d15ba827461208a7b4554f8d7203a2e7202ee
wwilliamyu <wwilliamyu@gmail.com>
2016-01-17 01:04:51 -0800

    adding more features to app

---
+++ ParseStarterProject-Swift/ParseStarterProject/MyProfile.swift
@@ -0,0 +1,40 @@
+/**
+* Copyright (c) 2015-present, Parse, LLC.
+* All rights reserved.
+*
+* This source code is licensed under the BSD-style license found in the
+* LICENSE file in the root directory of this source tree. An additional grant
+* of patent rights can be found in the PATENTS file in the same directory.
+*/
+
+import UIKit
+import Parse
+import Bolts
+
+class MyProfile: UIViewController {
+
+    @IBOutlet var profUsername: UITextField!
+    @IBOutlet var profPassword: UITextField!
+    @IBOutlet var profFirstName: UITextField!
+    @IBOutlet var profLastName: UITextField!
+    @IBOutlet var profPhone: UITextField!
+    @IBOutlet var profEmail: UITextField!
+    
+    @IBOutlet var currPassword: UITextField!
+    @IBAction func submitChangeProfile(sender: AnyObject) {
+        
+    
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
